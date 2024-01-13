abb590fdd64a97684db6ba2b70a1bf77cb927ca9
damiang19 <damiang19@gmail.com>
2024-01-13 18:03:08 +0100

    Initial commit

---
+++ src/main/java/pl/dgorecki/shop_scrapper/ShopScrapperApplication.java
@@ -0,0 +1,13 @@
+package pl.dgorecki.shop_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dgorecki/shop_scrapper/ShopScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dgorecki.shop_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
